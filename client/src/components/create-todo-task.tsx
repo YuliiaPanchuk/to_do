6cedc21b9d1d9a3799625f7b1528488553aa418a
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-12-11 22:45:51 +0100

    Add query to create task

--- client/src/components/create-todo-task.tsx
+++ client/src/components/create-todo-task.tsx
@@ -1,14 +1,40 @@
 import React, { useState } from 'react';
 
-export function CreateTodoList() {
+interface CreateTodoTaskProps {
+  list_id: number;
+}
+
+export function CreateTodoTask({ list_id }: CreateTodoTaskProps) {
   const [task, setTask] = useState('');
+  const [taskId, setTaskId] = useState('');
 
   function fetchTasks() {
-    fetch('http://localhost:3001/list/:id/task/', {
+    fetch(`http://localhost:3001/list/${list_id}/task/`, {
       method: 'POST',
-      body: JSON.stringify({}),
-    });
+      body: JSON.stringify({
+        task_name: task,
+        todo_id: taskId,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }).then((response) => response.json());
+
+    console.log({ fetchTasks });
   }
 
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Task name"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={fetchTasks}>
+        <i className="fa-regular fa-plus" />
+        New
+      </button>
+    </div>
+  );
 }
