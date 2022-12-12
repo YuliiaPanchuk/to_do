afd0402da1af5abc2823a3f213a7e225da7c93ab
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-12-12 09:25:56 +0100

    Display todos

--- client/src/components/show-todo-list.tsx
+++ client/src/components/show-todo-list.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import { CreateTodoTask } from './create-todo-task';
 import { DeleteToDoList } from './delete-todo-list';
 
 interface DisplayTodoListProps {
-  list: any[];
+  lists: any[];
   loading: boolean;
   onSuccess: () => void;
 }
 
-export function DisplayTodoList({ list, loading, onSuccess }: DisplayTodoListProps) {
+export function DisplayTodoList({ lists, loading, onSuccess }: DisplayTodoListProps) {
   return (
     <div className="showTodoWrapper">
       {loading && <p>A moment please...</p>}
-      {list.map((todo) => (
-        <div key={todo.id}>
-          <p>Displaying to do list: {todo.list_name}</p>
-          <DeleteToDoList onSuccess={onSuccess} id={todo.id} />
+      {lists.map((list) => (
+        <div key={list.id}>
+          <div className="showTodoList">
+            <p className="toDoListHeader">{list.name}</p>
+            <DeleteToDoList onSuccess={onSuccess} id={list.id} />
+          </div>
+          <div>
+            <CreateTodoTask list_id={list.id} />
+            <div>
+              {list.tasks.map((task: any) => (
+                <div key={task.id}>{task.name}</div>
+              ))}
+            </div>
+          </div>
         </div>
       ))}
     </div>
