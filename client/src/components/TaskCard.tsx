ff6a0a998d365a912bf608ae6e67c3d577b6568b
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-12-17 23:51:15 +0100

    Add Draggable component

--- client/src/components/TaskCard.tsx
+++ client/src/components/TaskCard.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { CreateSubtask } from './create-sub_task';
 import { DisplaySubtasks } from './display-sub_tasks';
+import Draggable, { DraggableCore } from 'react-draggable';
 
 interface TaskCardProps {
   task: any;
@@ -26,19 +27,21 @@ export function TaskCard({ task, onDelete, subtasks }: TaskCardProps) {
   }
 
   return (
-    <div className="flex flex-col rounded-xl shadow border p-8 my-6">
-      <input
-        className="w-40 font-semibold bg-transparent"
-        type="text"
-        value={tempTask}
-        placeholder={task.name}
-        onChange={(e) => {
-          setTempTask(e.target.value);
-        }}
-        onBlur={editTask}
-      />
-      <DisplaySubtasks subtasks={task.subtasks} onDelete={onDelete} />
-      <CreateSubtask task_id={task.id} />
-    </div>
+    <Draggable>
+      <div className="flex flex-col rounded-xl shadow border p-8 my-6">
+        <input
+          className="w-40 font-semibold bg-transparent"
+          type="text"
+          value={tempTask}
+          placeholder={task.name}
+          onChange={(e) => {
+            setTempTask(e.target.value);
+          }}
+          onBlur={editTask}
+        />
+        <DisplaySubtasks subtasks={task.subtasks} onDelete={onDelete} />
+        <CreateSubtask task_id={task.id} />
+      </div>
+    </Draggable>
   );
 }
