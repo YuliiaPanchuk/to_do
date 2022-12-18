4ecc5ab2715261472291f5d744c7eaaee2ec6544
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-12-18 21:38:16 +0100

    Update

--- client/src/components/display-sub_tasks.tsx
+++ client/src/components/display-sub_tasks.tsx
@@ -3,15 +3,14 @@ import { DeleteSubtask } from './delete-subtask';
 
 interface DisplaySubtasksProps {
   subtasks: any[];
-  onDelete: () => void;
 }
 
-export function DisplaySubtasks({ subtasks, onDelete }: DisplaySubtasksProps) {
+export function DisplaySubtasks({ subtasks }: DisplaySubtasksProps) {
   return (
     <div className="my-2">
       {subtasks.map((subtask) => (
         <div key={subtask.id} className="flex">
-          <DeleteSubtask onDelete={onDelete} id={subtask.id} />
+          <DeleteSubtask id={subtask.id} />
           <input type="text" value={subtask.sub_task_name} readOnly={true} />
         </div>
       ))}
