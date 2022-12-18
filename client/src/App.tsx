b01c2a9d90458c1aba1d6981eb931d2a257e7294
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-12-18 21:34:23 +0100

    Update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,56 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { Column } from './components/Column';
 import { CreateToDoList } from './components/create-todo-list';
-import { DisplayTodoList } from './components/show-todo-list';
+import { ListContext, ListProvider, useListContext } from './context/ListContext';
 
-export default function App() {
-  const [list, setList] = useState<any[]>([]); // display all todo list
-  const [loading, setLoading] = useState(false);
-  const [subtask, setSubtask] = useState<any[]>([]);
-
-  function fetchLists() {
-    fetch('http://localhost:3001/list', {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setList(data.result);
-      })
-      .catch((err) => {
-        console.log(`An error occur ${err}`);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }
-
-  useEffect(() => {
-    fetchLists();
-  }, []);
-
-  function fetchSubtasks() {
-    fetch('http://localhost:3001/list/task', {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setSubtask(data.result))
-      .catch((error) => console.log(`An error occured ${error}`));
-  }
-
-  useEffect(() => {
-    fetchSubtasks();
-  }, []);
+function App() {
+  const { lists } = useListContext();
 
   return (
     <div className="container mx-auto p-8 m-10">
       <div className="border-b border-indigo-500">All your todos are here</div>
       <CreateToDoList />
-      <DisplayTodoList lists={list} loading={loading} onSuccess={fetchLists} subtasks={subtask} />
+      <div className="flex">
+        {lists.map((list) => (
+          <Column key={list.id} list={list} />
+        ))}
+      </div>
     </div>
+  );
+}
+
+export default function AppWrapper() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <ListProvider>
+        <App />
+      </ListProvider>
+    </DndProvider>
   );
 }
