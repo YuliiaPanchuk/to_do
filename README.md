deba9b2dcefeb329abfe47bb16ecdd95d2be8f4f
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-12-18 21:43:47 +0100

    Update Readme file

--- README.md
+++ README.md
@@ -5,5 +5,3 @@
 To set up database run SQL file to create the MySQL database
 
 To start the server run the command "npm run dev" in server folder
-
-You can test the app using Postman
--- todo_app.sql
+++ todo_app.sql
@@ -2,28 +2,23 @@ DROP DATABASE IF EXISTS todo;
 CREATE DATABASE todo;
 USE todo;
 
-CREATE TABLE todo_user(
-	id int NOT NULL,
-    user_name varchar(255),
-    email varchar(255),
-    PRIMARY KEY (id),
-);
-
-CREATE TABLE todo_list(
-	id int NOT NULL,
-    list_name varchar(255),
-    owner_id int NOT NULL,
+CREATE TABLE status_list(
+	id int NOT NULL AUTO_INCREMENT,
+    status_name varchar(255),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE task(
-	id int NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
     task_name varchar(255),
-    tagged boolean,
-    completed boolean,
-    remind_at timestamp,
-    user_id int,
     todo_id int NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (todo_id) REFERENCES todo_list(id)
+    FOREIGN KEY (todo_id) REFERENCES status_list(id)
+);
+CREATE TABLE sub_task(
+	id int NOT NULL AUTO_INCREMENT,
+    sub_task_name varchar(255),
+    task_id int NOT NULL,
+	PRIMARY KEY (id),
+    FOREIGN KEY (task_id) REFERENCES task(id)
 );
