8562161a69f8f63535bb0f7a0b886f6912c3af55
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-12-20 15:34:05 +0100

    Create Delete to do component

---
+++ client/src/components/delete-todo-task.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useListContext } from '../context/ListContext';
+
+interface DeleteToDoTaskProps {
+  id: number;
+}
+
+export function DeleteToDoTask({ id }: DeleteToDoTaskProps) {
+  const { fetchLists } = useListContext();
+
+  function handleDelete() {
+    fetch(`http://localhost:3001/task/${id}`, {
+      method: 'DELETE',
+    }).then(fetchLists);
+  }
+
+  return (
+    <div onClick={handleDelete}>
+      <i className="fa-regular fa-trash-can fa-xs" />
+    </div>
+  );
+}
