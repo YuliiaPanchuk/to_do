bc4d306db57ef1c09c836b5f0a88acc448c100a0
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-12-22 15:45:48 +0100

    Create TaskSchema file

---
+++ server/models/TaskSchema.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+
+const TaskSchema = new mongoose.Schema({
+  _id: {
+    type: Number,
+  },
+  task_name: {
+    type: String
+  },
+  status_id: {
+    type: Number
+  },
+})
+export const Task = mongoose.model("Task", TaskSchema)
