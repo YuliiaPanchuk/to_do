fee0ae9753767970eea735cc6b4f10648d4c1aef
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2022-12-28 12:35:36 +0100

    Display subtasks

--- client/src/components/display-sub_tasks.tsx
+++ client/src/components/display-sub_tasks.tsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
+import { useListContext } from '../context/ListContext';
+import { SubtaskItem } from '../types';
 import { DeleteSubtask } from './delete-subtask';
 
 interface DisplaySubtasksProps {
-  subtasks: any;
+  subtasks: SubtaskItem[];
 }
 
 export function DisplaySubtasks({ subtasks }: DisplaySubtasksProps) {
   const [tempSubTask, setTempSubtask] = useState('');
+  const { fetchLists } = useListContext();
 
   function editSubtask(task_id: number) {
-    fetch(`http://localhost:3001/task/${subtasks.task_id}`, {
+    fetch(`http://localhost:3001/task/${task_id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        sub_task_name: tempSubTask,
+        name: tempSubTask,
         task_id,
       }),
       headers: {
@@ -20,15 +23,26 @@ export function DisplaySubtasks({ subtasks }: DisplaySubtasksProps) {
       },
     })
       .then((response) => response.json())
+      .then(() => {
+        setTempSubtask('');
+        fetchLists();
+      })
       .catch((error) => console.error(`An error occured ${error}`));
   }
 
   return (
     <div className="my-2">
-      {subtasks.map((subtask: any) => (
-        <div key={subtask.id} className="flex">
+      {subtasks.map((subtask: any, index: any) => (
+        <div key={index} className="flex">
           <DeleteSubtask id={subtask.id} />
-          <input type="text" value={subtask.sub_task_name} />
+          <input
+            type="text"
+            value={subtask.name}
+            onChange={(e) => {
+              setTempSubtask(e.target.value);
+            }}
+            onBlur={() => editSubtask(subtask.task_id)}
+          />
         </div>
       ))}
     </div>
