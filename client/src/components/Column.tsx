66b071e5206828bde6b838fce1162bf1f62f4810
Yuliia Panchuk <55352643+YuliiaPanchuk@users.noreply.github.com>
2023-01-04 15:15:45 +0100

    Add function for editing List

--- client/src/components/Column.tsx
+++ client/src/components/Column.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDrop } from 'react-dnd';
+import { useListContext } from '../context/ListContext';
+import { ListItem } from '../types';
 import { CreateTodoTask } from './create-todo-task';
 import { DeleteToDoList } from './delete-todo-list';
 import { TaskCard } from './TaskCard';
 
 interface ColumnProps {
-  list: any;
+  list: ListItem;
 }
 
 export function Column({ list }: ColumnProps) {
+  const [tempList, setTempList] = useState(list.name);
+  const { fetchLists } = useListContext();
+
   const [{ canDrop, isOver }, drop] = useDrop(() => ({
     accept: 'task',
     drop: () => list,
@@ -18,10 +23,32 @@ export function Column({ list }: ColumnProps) {
     }),
   }));
 
+  function editList() {
+    fetch(`http://localhost:3001/list/${list.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        list_name: tempList,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then(fetchLists)
+      .catch((error) => console.error(`An error occured ${error}`));
+  }
+
   return (
     <div className="min-w-[300px] m-2 h-auto border">
       <div className="flex my-1 ">
-        <p className="mr-1 text-xl">{list.name}</p> {/* contentEditable={true} */}
+        <input
+          className="mr-1 text-xl"
+          type="text"
+          value={list.name}
+          onChange={(e) => {
+            setTempList(e.target.value);
+          }}
+          onBlur={editList}
+        />
         <DeleteToDoList id={list.id} />
       </div>
 
@@ -29,9 +56,7 @@ export function Column({ list }: ColumnProps) {
         <CreateTodoTask list_id={list.id} />
         <div ref={drop} className="h-48">
           {list.tasks.map((task: any) => (
-            <div key={task.id}>
-              <TaskCard task={task} />
-            </div>
+            <TaskCard task={task} key={task.id} />
           ))}
         </div>
       </div>
